eda85accd5a5ab546f67b0c1ddd471842baa3613
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-29 13:27:06 -0700

    NTS: watching will not render newly created files accordingly

--- PierresVendorsAndOrders/Controllers/VendorsController.cs
+++ PierresVendorsAndOrders/Controllers/VendorsController.cs
@@ -34,5 +34,17 @@ namespace PierresVendorsAndOrders.Controllers
       model.Add("orders", vendorOrders);
       return View(model);
     }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string productName)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(productName);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
