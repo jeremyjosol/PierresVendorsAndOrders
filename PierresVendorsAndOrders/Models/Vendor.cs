ffae8be569d9812f691377fa0ad78933406998a3
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-29 11:21:52 -0700

    Create Vendor constructor + relevant methods

--- PierresVendorsAndOrders/Models/Vendor.cs
+++ PierresVendorsAndOrders/Models/Vendor.cs
@@ -8,5 +8,24 @@ namespace PierresVendorsAndOrders.Models
     private static List<Vendor> _instances = new List<Vendor> { };
     public string Name { get; set; }
     public int Id { get; }
+
+    public Vendor(string businessName)
+    {
+      Name = businessName;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
   }
 }
