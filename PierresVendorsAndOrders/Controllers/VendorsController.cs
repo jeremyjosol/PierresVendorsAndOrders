4dd6b3ebcb6e6b4da148aec3e2f8a3ccaa4ca1ea
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-29 12:49:24 -0700

    Add Create post route + redirect to Index

--- PierresVendorsAndOrders/Controllers/VendorsController.cs
+++ PierresVendorsAndOrders/Controllers/VendorsController.cs
@@ -18,5 +18,11 @@ namespace PierresVendorsAndOrders.Controllers
     {
       return View();
     }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+    Vendor newVendor = new Vendor(vendorName);
+    return RedirectToAction("Index");
+    }
   }
 }
