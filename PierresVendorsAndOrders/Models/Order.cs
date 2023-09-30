c18d1935221fb490d40076cc0894bdcce3c50640
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-29 22:56:34 -0700

    Add remaining properties

--- PierresVendorsAndOrders/Models/Order.cs
+++ PierresVendorsAndOrders/Models/Order.cs
@@ -7,12 +7,22 @@ namespace PierresVendorsAndOrders.Models
     public string Product { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Date { get; set; }
 
     public Order(string product)
     {
       Product = product;
       _instances.Add(this);
       Id = _instances.Count;
+    }
+    public Order(string product, string productDescription, decimal productPrice, string orderDate)
+    {
+      Product = product;
+      Description = productDescription;
+      Price = productPrice;
+      Date = orderDate;
     }
     public static List<Order> GetAll()
     {
